c5f64f1015217203c70f56c41727b5337b9f48eb
maryamalrubaye <maryamalrubaye18@gmail.com>
2022-05-08 08:50:50 +0300

    login/signup system with database connection

--- message_session.py
+++ message_session.py
@@ -1,9 +1,11 @@
 import json
 import time
 import redis
-
+import sqlite3
+import sys
 from typing import Tuple
 from threading import Thread
+import itertools
 
 """
 private chat: 
@@ -16,11 +18,106 @@ all people wishing to use public chat should:
     
     
 TODO:
-- filter user subscribed message to be as same as the publisher name. (for the private chat)
-- group chat
-- login/signup (id, username, password)
+- filter user subscribed message to be as same as the publisher name. (for the private chat) done
+- group chat done
+- login/signup (id, username, password) done
 - every user chat history
 """
+
+
+class LoginHandler:
+    connection = sqlite3.connect("login.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS login (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL UNIQUE,email TEXT NOT NULL UNIQUE,password TEXT NOT NULL)")
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS GroupTable (name TEXT NOT NULL)")
+
+    connection.commit()
+    usernames = cursor.execute("SELECT name FROM login").fetchall()
+    username_list = list(itertools.chain(*[username for username in usernames]))
+    groups = cursor.execute("SELECT name FROM GroupTable").fetchall()
+    group_list = list(itertools.chain(*[group for group in groups]))
+
+    @classmethod
+    def check_username_existing(cls, username) -> bool:
+        existed_username = LoginHandler.cursor.execute(
+            "SELECT name FROM login WHERE name='" + username + "'").fetchone()
+        existed_username = str(existed_username).strip("('',)'")
+        if existed_username == username:
+            return True
+
+    @classmethod
+    def check_email_existing(cls, email) -> bool:
+        existed_email = LoginHandler.cursor.execute("SELECT email FROM login WHERE email='" + email + "'").fetchone()
+        existed_email = str(existed_email).strip("('',)'")
+        if existed_email == email:
+            return True
+
+
+class Registration:
+    def __init__(self):
+        self.username = None
+        self.password = None
+        self.email = None
+        self.rewrite_password = None
+        self.start_login()
+
+    def start_login(self) -> None:
+        while True:
+            self.username = input("Enter your username. ")
+            if LoginHandler.check_username_existing(self.username):
+                print('That username already exists,try another one!')
+                continue
+            else:
+                while True:
+                    self.email = input("Enter your email. ")
+                    if LoginHandler.check_email_existing(self.email):
+                        print('That email is already in our database,enter another one!')
+                        continue
+                    else:
+                        while True:
+                            self.password = input("Enter your password. ")
+                            self.rewrite_password = input("Enter your password again. ")
+                            if self.__check_password():
+                                sys.exit()
+                            continue
+
+    def __check_password(self) -> bool:
+        if self.password == self.rewrite_password:
+            LoginHandler.cursor.execute('INSERT INTO login VALUES(?,?,?,?)',
+                                        (None, self.username, self.email, self.password))
+            LoginHandler.connection.commit()
+            print('You are now registered.')
+            return True
+
+        else:
+            print('Password does not match')
+            return False
+
+
+class Login:
+    def __init__(self, username: str):
+        self.password = None
+        self.username = username
+
+    def start(self) -> bool:
+        cursor = LoginHandler.cursor
+        check_user = False
+        while True:
+            password = input("Enter your password. ")
+            if LoginHandler.check_username_existing(self):
+                db_password = cursor.execute("SELECT password from login WHERE password='" + password + "'").fetchone()
+                db_password = str(db_password).strip("('',)'")
+                if db_password == password:
+                    print('You are now logged in.')
+                    check_user = True
+                    return check_user
+                else:
+                    print('Wrong password.')
+            else:
+                print('Wrong username.')
+                return check_user
 
 
 class MessageHandler:
@@ -29,8 +126,8 @@ class MessageHandler:
     redis_client = redis.Redis(host='localhost', port=port, password=password)
 
     channels = {
-        'private': ['maryam', 'huyam', 'hala'],
-        'public': ['group1'],
+        'private': LoginHandler.username_list,
+        'public': LoginHandler.group_list,
     }
 
     @classmethod
@@ -143,8 +240,17 @@ class MessageSession:
         self.recipient = None
 
     def get_user_info(self) -> None:
-        self.username: str = str(input("Enter your username: "))
-        self.recipient: str = str(input("Enter your Recipient: "))
+        query = input('for login please write 1 & for signup please write 2')
+        if query == "1":
+            while True:
+                self.username: str = str(input("Enter your username: "))
+                if Login.start(self.username):
+                    break
+            self.recipient: str = str(input("Enter your Recipient: "))
+        elif query == "2":
+            Registration()
+        else:
+            print('Incorrect input.Run script again. ')
 
     def start_messaging_session(self):
         """ Starts the messaging session.
@@ -160,11 +266,7 @@ class MessageSession:
     def __check_if_channel_exist(self) -> bool:
         channel_exist = True
         all_channels = MessageHandler.get_all_channels()
-        private_channels = MessageHandler.get_private_channels()
-
-        if self.username not in private_channels:
-            print(f"User '{self.username}' does not exist!")
-            channel_exist = False
+        print(all_channels)
         if self.recipient not in all_channels:
             print(f"Recipient '{self.recipient}' does not exist!")
             channel_exist = False
