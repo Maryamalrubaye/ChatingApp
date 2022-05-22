a477a44fe9b1496ec9b17144dfab9599553f1bf4
maryamalrubaye <maryamalrubaye18@gmail.com>
2022-05-22 17:37:45 +0300

    Added user interface.

--- app_controller.py
+++ app_controller.py
@@ -1,5 +1,7 @@
+
 import login_system
 import message_session
+import chat_history
 
 
 class AppController:
@@ -11,14 +13,14 @@ class AppController:
     def get_user_info(self) -> None:
         """ check if user is registered or not and login to user account or creates new account
                """
-        choice = input('for login please write 1 & for signup please write 2 : ')
-        if choice == "1":
+        self.choice = input('for login please write 1 & for signup please write 2 : ')
+        if self.choice == "1":
             self.username = login_system.Login().login()
-            self.__get_recipient_info()
-        elif choice == "2":
+            self.__main_page()
+        elif self.choice == "2":
             username, password = login_system.Registration().register()
             self.username = login_system.Login().login(username, password)
-            self.__get_recipient_info()
+            self.__main_page()
         else:
             print('Incorrect input! Please try again.')
             self.get_user_info()
@@ -26,6 +28,29 @@ class AppController:
     def __get_recipient_info(self) -> None:
         self.recipient: str = str(input("Enter your recipient: "))
         message_session.MessageSession(self.username, self.recipient)
+
+    def __main_page(self):
+        print(f'Welcome {self.username} you have contacted the following channels: ')
+        chat_history.ConversationListHandler(self.username)
+        self.choice = input('1- to create a new group  \n'
+                            '2- to search for new user \n'
+                            '3- choose any of the previously contacted channels \n'
+                            '4- to exit: ')
+        if self.choice == '1':
+            self.__group_creator()
+        elif self.choice in ['2', '3']:
+            self.__get_recipient_info()
+        elif self.choice == '4':
+            print('bye bye sweet one :) ')
+            exit()
+
+    def __group_creator(self) -> None:
+        group_name: str = str(input("Enter a group name: "))
+        members = input('Enter elements of a list separated by space ')
+        print("\n")
+        members_list = members.split()
+        message_session.GroupCreator(members_list, group_name)
+        self.__main_page()
 
 
 if __name__ == '__main__':
